2119c30cc605622d5f27dac697aeeea854a39a67
Tonpai2004 <TonpaiTc@gmail.com>
2025-04-01 00:55:26 +0700

    Responsive M1_FlashCard, M1_Ask(not finish) , Edit Character in backgrounf Home. Menu. Mainmenu

--- FlashCard/Program.cs
+++ FlashCard/Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=FlashCard}/{action=M1_Enhance1}/{id?}")
+    pattern: "{controller=FlashCard}/{action=M1_Ask}/{id?}")
     .WithStaticAssets();
 
 
