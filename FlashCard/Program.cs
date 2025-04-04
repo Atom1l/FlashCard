3657627f9b65fb86ba3744a4b35bf2685bb65cef
atom <atom7548@gmail.com>
2025-04-04 10:29:02 +0700

    Update : fix-music name

--- FlashCard/Program.cs
+++ FlashCard/Program.cs
@@ -56,7 +56,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=FlashCard}/{action=Home}/{id?}")
+    pattern: "{controller=FlashCard}/{action=M1_Enhance1}/{id?}")
     .WithStaticAssets();
 
 app.Run();
